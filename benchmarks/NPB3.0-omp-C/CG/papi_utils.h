bc9fb9798f3361bf4e947b22b501a6b29fb1a0e6
hvzzzz <hananquispec@outlook.com>
2026-01-26 23:26:23 +0100

    added all bitstreams and usage reports

--- benchmarks/NPB3.0-omp-C/CG/papi_utils.h
+++ benchmarks/NPB3.0-omp-C/CG/papi_utils.h
@@ -2,9 +2,10 @@
 #ifndef PAPI_UTILS_H
 #define PAPI_UTILS_H
 
-#include "papi.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "papi.h"
 
 #define CVA6_INS 0x80000032
 #define CVA6_CYC 0x8000003b
@@ -13,22 +14,33 @@ static int EventSet = PAPI_NULL;
 static long long values[2];
 
 void papi_init_and_start() {
-    if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT)
-        exit(1);
-    PAPI_create_eventset(&EventSet);
+  int retval = 0;
+  if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT) exit(1);
+  PAPI_create_eventset(&EventSet);
 
-    PAPI_set_domain(PAPI_DOM_ALL);
+  PAPI_set_domain(PAPI_DOM_ALL);
 
-    PAPI_add_event(EventSet, CVA6_INS);
-    PAPI_add_event(EventSet, CVA6_CYC);
-    PAPI_start(EventSet);
+  retval = PAPI_add_event(EventSet, CVA6_INS);
+  if (retval != PAPI_OK) {
+    fprintf(stderr, "Error adding event %s: %s\n", event_names[i],
+            PAPI_strerror(retval));
+    // Optional: Continue even if one fails, or exit
+  }
+  retval = PAPI_add_event(EventSet, CVA6_CYC);
+  if (retval != PAPI_OK) {
+    fprintf(stderr, "Error adding event %s: %s\n", event_names[i],
+            PAPI_strerror(retval));
+    // Optional: Continue even if one fails, or exit
+  }
+  /* retval = PAPI_add_event(EventSet, test); */
+  PAPI_start(EventSet);
 }
 
 void papi_stop_and_print() {
-    PAPI_stop(EventSet, values);
-    printf("\n[PAPI] Instructions: %lld\n", values[0]);
-    printf("[PAPI] Cycles:       %lld\n", values[1]);
-    if (values[1] > 0)
-        printf("[PAPI] IPC:          %.4f\n", (double)values[0] / values[1]);
+  PAPI_stop(EventSet, values);
+  printf("\n[PAPI] Instructions: %lld\n", values[0]);
+  printf("[PAPI] Cycles:       %lld\n", values[1]);
+  if (values[1] > 0)
+    printf("[PAPI] IPC:          %.4f\n", (double)values[0] / values[1]);
 }
 #endif
